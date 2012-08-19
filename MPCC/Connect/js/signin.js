1db4e4ca011a77937b4f51a4656848757a5d7951
iulianmihai <imihai@exacttarget.com>
2012-08-19 09:36:36 -0400

    Minimized all the CSS in bootstrap.min.css.Login code in signin.js script

---
+++ MPCC/Connect/js/signin.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $("#btnSignIn").click(function () {
+        $.ajax({
+            url: "http://localhost/Rest2/Auth/v1/tokenrequest",
+            type: 'POST',
+            contentType: 'application/json',
+            data: { username: $("#username").val(), password: $("#password").val() },
+            success: function () {
+                document.location = 'home.html';
+            },
+            error: function (err) {
+                console.log('error logging in');
+            }
+        });
+    });
+    $('input').keydown(function (e) {
+        if (e.keyCode == 13) {
+            $('#btnSignIn').click();
+            return false;
+        }
+    });
+    $('#username').focus();
+});
