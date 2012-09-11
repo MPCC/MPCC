cbe12ddd1b02e75f0392b85df90fd32185796e2b
Iulian Mihai <imihai@exacttarget.com>
2012-09-10 22:03:03 -0400

    App Architecture after user is authenticate (Loading RequireJs, jQuery, Bootstrap, Undersore, Backbone, Order)

---
+++ MPCC/Connect/js/loader.js
@@ -0,0 +1,8 @@
+﻿define(['order!libs/jquery', 'order!libs/bootstrap', 'order!libs/underscore', 'order!libs/backbone'],
+function () {
+    return {
+        Backbone: Backbone.noConflict(),
+        _: _.noConflict(),
+        $: jQuery.noConflict()
+    };
+});
--- MPCC/Connect/js/main.js
+++ MPCC/Connect/js/main.js
@@ -1,24 +1,13 @@
 require.config({
     paths: {
-        'underscore': 'underscore',
-        'backbone': 'backbone'
+        loader: 'loader',
+        jQuery: 'libs/jquery',
+        Bootstrap: 'libs/bootstrap', 
+        Underscore: 'libs/underscore',
+        Backbone: 'libs/backbone',
+        templates:'../templates'
     }
 });
-require(["jquery", "app"], function ($, app) {
-    
-
-    $(document).ajaxError(function(e, xhr, options) {
-        switch (xhr.status) {
-            case 401:
-                document.location = 'index.html';
-                break;
-        }
-    });
-
-    //    $('body').ajaxComplete(function (event, xhr, opts) {
-    //        if (opts.url != 'http://localhost/Rest2/sessionkeep-alive') {
-    //            //session.ExtendSession();
-    //        }
-    //    });
-
+require(['app'], function (App) {
+    App.initialize();
 });
